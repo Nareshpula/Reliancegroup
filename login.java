5102ec53b03a68b34706bfe049605a272aa914d0
Naresh <Nareshroyal@gmail.com>
2017-06-15 23:11:44 -0700

    i commited by the new file called login.java

---
+++ login.java
@@ -0,0 +1,8 @@
+this is the best way to give this job.
+they can give any thing more happen to the file.
+the login.java this is the file to join papulation.
+maintain the papulation.
+N = 12345
+this the value they can easy to access to the file
+they will given by the file.
+
