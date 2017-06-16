e615f8ec2b787b58c5a8d5faf915cc390d15730e
Naresh <Nareshroyal@gmail.com>
2017-06-15 23:17:56 -0700

    i am change the data from login.java

--- login.java
+++ login.java
@@ -2,7 +2,7 @@ this is the best way to give this job.
 they can give any thing more happen to the file.
 the login.java this is the file to join papulation.
 maintain the papulation.
-N = 12345
+N = 8520 it is the file to take the data.
 this the value they can easy to access to the file
 they will given by the file.
 
