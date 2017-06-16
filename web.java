2af3003db54bc31286c55ccccd19fe817d67c678
Naresh <Nareshroyal@gmail.com>
2017-06-15 23:15:10 -0700

    i commited by the web.java file

---
+++ web.java
@@ -0,0 +1,6 @@
+this is the file called web.java they can easy to pay the file.
+to access to pay the maintain the file.
+maintain to capable to the maintain the file.
+N = 2360.
+this is the file capable for the web.java.
+
