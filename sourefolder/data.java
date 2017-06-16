cbae1a904eb6aef2d03eba86b02ab8f5baa282f3
Naresh <Nareshroyal@gmail.com>
2017-06-15 23:36:00 -0700

    i can change the data in file

--- sourefolder/data.java
+++ sourefolder/data.java
@@ -1,3 +1,7 @@
 this is the file to capable to the maintain the data file.
 they can give any access to the accupy to file maintain the data.
 they can easy to the file.
+i can something the change the file.
+this is the file can easy to change the data file.
+it is the capable for the this is the action.
+
